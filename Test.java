0eaf6dc4b075f599efd2252d92daccd2fd9ed168
lheng <320480886@qq.com>
2018-07-02 21:17:01 +0800

    我的第一个Maven项目 基本骨架

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.baizhi.test;
+
+import java.awt.*;
+import java.util.Date;
+
+/**
+ * Created by mjli on 2018/7/1.
+ */
+public class Test {
+    @org.junit.Test
+    public void test1(){
+        System.out.println(new Date());
+    }
+}
